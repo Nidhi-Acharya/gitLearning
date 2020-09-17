50394a56d649a66c58a92085094f50652a677f7f
nidz <nidhiacharya2199@gmail.com>
2020-09-17 23:12:55 +0530

    initial commit

---
+++ 1.py
@@ -0,0 +1,18 @@
+from numpy import *
+
+var=array([1,7,0,8])
+var2=array([78,12,1,6])
+var3=zeros(len(var))
+print(var3)
+max=0
+for i in var:
+    if(i>max):
+        max=i
+
+print(max)
+
+j=0
+for i in var:
+    var3[j]=var2[j]+i
+    j+=1
+print(var3)
